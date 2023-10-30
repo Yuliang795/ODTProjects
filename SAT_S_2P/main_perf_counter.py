f85d30f25759fe860e281d3654d9e40982ace136
Yuliang795 <yl.song@mail.utoronto.ca>
2023-10-30 15:37:44 -0400

    jar_v11

--- SAT_S_2P/main_perf_counter.py
+++ SAT_S_2P/main_perf_counter.py
@@ -38,9 +38,11 @@ consts_df = pd.concat([consts_df, pd.DataFrame(consts_list, columns = consts_df.
 file_list = consts_path_query(consts_df,
                                     in_data=[],
                                     in_seed=[],
-                                    in_kappa=[0.1,0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5]) # kappa cannot be 0.0 must >0
+                                    in_kappa=[0.1,0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5]) 
+                                    # kappa cannot be 0.0 must >0
+                                    #0.1,0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5
 use_Chain=["nochain","euc"][0]
-ML_CL_ratio_set = ["1", "2", "3", "5", "10"]
+ML_CL_ratio_set = ["1"]
 stage1_timeout, stage2_timeout = [1800, 1800]
 
 for ML_CL_ratio in ML_CL_ratio_set:
